6b2b286f98673366c6088cdd75e7a04a602b78e4
fatemeh khoshbayan <f.khoshbayan@gmail.com>
2026-07-10 22:58:30 -0400

    feat: add Wall component with HTML template and unit tests for component creation

---
+++ src/app/features/wall/wall.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wall',
+  standalone: true,
+  imports: [],
+  templateUrl: './wall.html',
+  styles: ``,
+})
+export class Wall {}
